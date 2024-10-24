8476d0ae00ee3275fe9b1c871c8becf827ba1fa8
LafRina <lafenkoirina5@gmail.com>
2024-10-24 22:07:02 +0000

    Implement Taylor series for cosine

--- Func.cpp
+++ Func.cpp
@@ -1,7 +1,12 @@
 #include "func.h"
+#include <cmath>
 
 double FuncA::compute(double x, int n) {
-    // Повертаємо 0 поки що
-    return 0;
+    double sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += pow(-1, i) * pow(x, 2*i) / tgamma(2*i + 1);  // tgamma(2*i + 1) це (2*i)!
+    }
+    return sum;
 }
+
 
