d2f26cad2c402e2b5ac2e45f064fa6db7a3c1a76
LafRina <lafenkoirina5@gmail.com>
2024-10-24 22:24:43 +0000

    add comments

--- Func.cpp
+++ Func.cpp
@@ -3,6 +3,7 @@
 
 double FuncA::compute(double x, int n) {
     double sum = 0;
+    // Цикл для обчислення суми перших n елементів
     for (int i = 0; i < n; ++i) {
         sum += pow(-1, i) * pow(x, 2*i) / tgamma(2*i + 1);  // tgamma(2*i + 1) це (2*i)!
     }
